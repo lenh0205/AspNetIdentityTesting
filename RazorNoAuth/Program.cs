478be4e94e5d5706f09bd71b407b2e8df689c444
vietinfo123 <lenhtranchi98@gmail.com>
2024-09-12 13:10:09 +0700

    after Identity Scaffold

--- RazorNoAuth/Program.cs
+++ RazorNoAuth/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RazorNoAuth.Areas.Identity.Data;
+using RazorNoAuth.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -13,6 +24,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
